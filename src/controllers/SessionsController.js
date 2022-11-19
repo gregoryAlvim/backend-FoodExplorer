d350f9924b284de34bc0a230ac55685433766072
Grégory Alvim <gregori.jagua@gmail.com>
2022-11-18 23:46:51 -0300

    Feat: created SessionController

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,34 +1,16 @@
-const { compare } = require('bcrypt');
-const { sign } = require('jsonwebtoken');
-
-const knex = require('../database/knex');
-const authConfig = require('../config/auth');
-const AppError = require('../utils/AppError');
+const UserRepository = require('../repositories/UserRepository');
+const SessionCreateService = require('../services/SessionCreateService');
 
 class SessionsController {
    async create(request, response) {
       const { email, password } = request.body;
 
-      const user = await knex("users").where({ email }).first();
+      const userRepository = new UserRepository();
+      const sessionCreateService = new SessionCreateService(userRepository);
 
-      if (!user) {
-         throw new AppError(" E-mail e/ou senha incorretos! ", 401);
-      }
-
-      const matchedPassword = await compare(password, user.password);
+      const userWithToken = await sessionCreateService.execute({ email, password });
       
-      if (!matchedPassword) {
-         throw new AppError(" E-mail e/ou senha incorretos! ", 401);
-      }
-
-      const { secret, expiresIn } = authConfig.jwt;
-
-      const token = sign({}, secret, {
-         subject: String(user.id),
-         expiresIn
-      });
-      
-      return response.json({ user, token });
+      return response.json(userWithToken);
    }
 }
 
