a16e5fe5b70ecf445e157987b1a347ba8b131f35
Grégory Alvim <gregori.jagua@gmail.com>
2022-11-26 19:31:52 -0300

    Feat: created roles controller

--- src/controllers/RolesController.js
+++ src/controllers/RolesController.js
@@ -1,5 +1,17 @@
-class RolesController {
+const RoleRepository = require('../repositories/RoleRepository');
+const RoleCreateService = require('../services/RoleCreateService');
 
+class RolesController {
+   async create(request, response) {
+      const { name, description } = request.body;
+
+      const roleRepository = new RoleRepository();
+      const roleCreateService = new RoleCreateService(roleRepository);
+
+      await roleCreateService.execute({ name, description });
+
+      return response.status(201).json("Função cadastrada com sucesso!");
+   }
 }
 
 module.exports = RolesController;
