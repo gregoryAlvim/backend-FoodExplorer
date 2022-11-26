1a4fc0dd4b7b9e25f7e41c29c4b72f6ed0a299ea
Grégory Alvim <gregori.jagua@gmail.com>
2022-11-26 19:29:54 -0300

    Feat: added roles routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 
 const usersRouter = require('./users.routes');
 const sessionsRouter = require('./sessions.routes');
+const rolesRouter = require('./roles.routes');
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
+routes.use("/roles", rolesRouter);
 
 module.exports = routes;
