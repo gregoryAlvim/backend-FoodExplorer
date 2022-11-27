5a19ae5ca91fac0d6065b78b16f9e76ad447db74
Grégory Alvim <gregori.jagua@gmail.com>
2022-11-27 19:20:41 -0300

    Feat: created relationship with the user and her role

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -5,13 +5,26 @@ class UserRepository {
       const userExist = await knex("users").where({ email }).first();
 
       return userExist;
-   }
+   };
 
-   async create({name, email, password}) {
+   async create({ name, email, password }) {
       const userId = await knex("users").insert({ name, email, password });
 
-      return { id: userId };
-   }
+      return { userId: userId };
+   };
+
+   async linkUserToRole({ userId }) {
+      const { id: roleId } = await knex("roles").where({ name: "User" }).first();
+      
+      await knex("users_roles").insert({ userId, roleId });
+   };
+
+   async findUserRole(email) {
+      const { id:userId } = await knex("users").where({ email }).first();
+      const { name:userRole } = await knex.from("users_roles").innerJoin("roles", "roleId", "id").where({ userId }).first();
+      
+      return userRole;
+   };
 };
 
 module.exports = UserRepository;
