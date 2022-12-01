948994fe4dc83bb1f280995d2520041b3f4dd409
Grégory Alvim <gregori.jagua@gmail.com>
2022-11-30 22:34:33 -0300

    Refactor: added two new coluns in the role migration

--- src/database/knex/migrations/20221120141214_createRoles.js
+++ src/database/knex/migrations/20221120141214_createRoles.js
@@ -1,8 +1,10 @@
 exports.up = knex => knex.schema.createTable("roles", table => {
    table.increments("id");
+   
    table.text("name").notNullable();
+   table.text("description").notNullable();
 
-   table.integer("user_id").references("id").inTable("users");
+   table.timestamp("created_at").defaultTo(knex.fn.now());
 });
 
 exports.down = knex => knex.schema.dropTable("roles");
