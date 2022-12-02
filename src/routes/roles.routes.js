57ca357574f63daee36d8cac0ae0e23a7e4bbc76
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-01 22:53:51 -0300

    Feat: tests permissions middleware

--- src/routes/roles.routes.js
+++ src/routes/roles.routes.js
@@ -3,9 +3,10 @@ const { Router } = require('express');
 const RolesController = require('../controllers/RolesController');
 const rolesController = new RolesController();
 const ensuredAuthenticated = require('../middlewares/ensuredAuthenticated');
+const { can } = require('../middlewares/ensuredPermissions');
 
 const rolesRoutes = new Router();
 
-rolesRoutes.post('/', ensuredAuthenticated, rolesController.create);
+rolesRoutes.post('/', ensuredAuthenticated, can, rolesController.create);
 
 module.exports = rolesRoutes;
