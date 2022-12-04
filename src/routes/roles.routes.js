15468520274dd4a48a34c6091e6c16d4eb496417
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-04 18:30:45 -0300

    Feat: added function who can access the route

--- src/routes/roles.routes.js
+++ src/routes/roles.routes.js
@@ -3,10 +3,10 @@ const { Router } = require('express');
 const RolesController = require('../controllers/RolesController');
 const rolesController = new RolesController();
 const ensuredAuthenticated = require('../middlewares/ensuredAuthenticated');
-const { can } = require('../middlewares/ensuredPermissions');
+const whoCanAccess = require('../middlewares/ensuredPermissions');
 
 const rolesRoutes = new Router();
 
-rolesRoutes.post('/', ensuredAuthenticated, can, rolesController.create);
+rolesRoutes.post('/', ensuredAuthenticated, whoCanAccess(["Admin"]), rolesController.create);
 
 module.exports = rolesRoutes;
