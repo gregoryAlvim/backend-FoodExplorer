9d207561900c4f9144e35bfc6baab1397e9e2004
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-06 22:09:16 -0300

    Feat: created knex seed to insert default roles

---
+++ src/database/knex/seeds/insertDefaultRoles.js
@@ -0,0 +1,10 @@
+exports.seed = async (knex) => {
+
+  await knex('roles').del();
+  await knex('users').del();
+
+  await knex('roles').insert([
+    {name: "Admin", description: "Can access the all pages of app"},
+    {name: "User", description: "Can access the all pages user of app"},
+  ]);
+};
