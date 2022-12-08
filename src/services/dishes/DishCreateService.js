84638ac3a9090d19a94549ec3bb9d9219976030c
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-07 21:23:04 -0300

    Feat: created service to create a dish on db

---
+++ src/services/dishes/DishCreateService.js
@@ -0,0 +1,22 @@
+const AppError = require('../../utils/AppError');
+
+class DishCreateService {
+   constructor(dishRepository) {
+      this.dishRepository = dishRepository;
+   }
+
+   async execute({ name, description, price }) {
+
+      if (!name || !description || !price) {
+         throw new AppError("Todos os campos são obrigatórios!");
+      } else if (price <= 0) {
+         throw new AppError("O valor do prato deve ser maior que 0.00 R$");
+      }
+
+      this.dishRepository.create({ name, description, price });
+
+   }
+
+}
+
+module.exports = DishCreateService;
