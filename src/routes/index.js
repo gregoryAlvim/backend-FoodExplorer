0f8267c430aeb245d5de159a1582a4939aa3138d
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-07 21:22:09 -0300

    Feat: added dishes routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require('express');
 const usersRouter = require('./users.routes');
 const sessionsRouter = require('./sessions.routes');
 const rolesRouter = require('./roles.routes');
+const dishesRouter = require('./dishes.routes');
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
 routes.use("/roles", rolesRouter);
+routes.use("/dishes", dishesRouter);
 
 module.exports = routes;
