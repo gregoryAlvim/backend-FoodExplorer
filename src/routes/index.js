5d8ac7ff5712012c7a230d9e6f94681f1c53392c
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-27 17:29:26 -0300

    Feat: added ingredients routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const usersRouter = require('./users.routes');
 const sessionsRouter = require('./sessions.routes');
 const rolesRouter = require('./roles.routes');
 const dishesRouter = require('./dishes.routes');
+const ingredientsRouter = require('./ingredients.routes');
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
 routes.use("/roles", rolesRouter);
 routes.use("/dishes", dishesRouter);
+routes.use("/ingredients", ingredientsRouter);
 
 module.exports = routes;
