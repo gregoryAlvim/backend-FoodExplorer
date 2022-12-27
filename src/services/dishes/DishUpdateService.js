d0a10b40b2d596b310693ce2f4e3b204b0601cb2
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-27 17:33:50 -0300

    Refactor: added update ingredients

--- src/services/dishes/DishUpdateService.js
+++ src/services/dishes/DishUpdateService.js
@@ -5,8 +5,8 @@ class DishUpdateService {
       this.dishRepository = dishRepository;
    }
 
-   async execute({ id:dishId, name, description, price, ingredients }) {
-      const dishData = await this.dishRepository.show({ dishId });
+   async execute({ id:dishId, name, description, category, price, ingredients }) {
+      const { dishData, ingredientsData} = await this.dishRepository.show({ dishId });
 
       if (!dishData) {
          throw new AppError("O prato não foi encontrado!");
@@ -14,15 +14,16 @@ class DishUpdateService {
 
       const dishName = name ?? dishData.name;
       const dishDescription = description ?? dishData.description;
+      const dishCategory = category ?? dishData.category;
       const dishPrice = price ?? dishData.price;
       
-      const ingredientsArray = dishData.ingredientsData.map( ingredient => {
+      const ingredientsArray = ingredientsData.map( ingredient => {
          return ingredient.name;
       });
 
       const dishIngredients = ingredients ?? ingredientsArray;
 
-      await this.dishRepository.update({ dishId, dishName, dishDescription, dishPrice, dishIngredients })
+      await this.dishRepository.update({ dishId, dishName, dishDescription, dishCategory, dishPrice, dishIngredients });
    };
 }
 
