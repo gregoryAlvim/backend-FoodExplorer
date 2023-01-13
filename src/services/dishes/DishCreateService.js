7f1981ca6513c90d0617dffa421943df19a0fe23
Grégory Alvim <gregori.jagua@gmail.com>
2023-01-13 14:54:54 -0300

    Feat: added new role

--- src/services/dishes/DishCreateService.js
+++ src/services/dishes/DishCreateService.js
@@ -7,7 +7,7 @@ class DishCreateService {
 
    async execute({ name, description, category, price, ingredients }) {
 
-      if (!name || !description || !category || !price) {
+      if (!name || !description || !category || !price || ingredients[0] == null) {
          throw new AppError("Todos os campos são obrigatórios!");
       } else if (price <= 0) {
          throw new AppError("O valor do prato deve ser maior que 0.00 R$");
